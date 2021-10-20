9c94734199ca0908835d4137d21fe5fadbba01c9
suicidevladick <borisevich.vlad98@gmail.com>
2021-10-20 15:21:26 +0300

    autobuild

--- main.py
+++ main.py
@@ -1,79 +1,61 @@
-from kivymd.app import MDApp
 from kivy.lang import Builder
-from kivymd.toast import toast
-from kivymd.uix.bottomsheet import MDListBottomSheet
-from kivymd.uix.label import MDLabel
+
+from kivymd.app import MDApp
+from kivymd.uix.tab import MDTabsBase
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.icon_definitions import md_icons
 
 KV = '''
-MDScreen:
-
-    orientation:'vertical'
-
-    MDToolbar:
-        title: 'Bottom navigation'
-        md_bg_color: .2, .2, .2, 1
-        specific_text_color: 1, 1, 1, 1
-
-    MDBottomNavigation:
-        panel_color: .2, .2, .2, 1
-
-        MDBottomNavigationItem:
-            name: 'screen 1'
-            text: 'Sanya Loh'
-            icon: 'language-python'
-
-            MDLabel:
-                text: 'Python'
-                halign: 'center'
-
-        MDBottomNavigationItem:
-            name: 'screen 2'
-            text: 'C++'
-            icon: 'language-cpp'
-
-            MDLabel:
-                text: 'I programming of C++'
-                halign: 'center'
-
-        MDBottomNavigationItem:
-            name: 'screen 3'
-            text: 'JS'
-            icon: 'language-javascript'
-
-            MDLabel:
-                text: 'JS'
-                halign: 'center'
-
+MDBoxLayout:
+    orientation: "vertical"
 
     MDToolbar:
-        title: "Example BottomSheet"
-        pos_hint: {"top": 1}
-        elevation: 10
+        title: "Example Tabs"
 
-    MDRaisedButton:
-        text: "Open list bottom sheet"
-        on_release: app.show_example_list_bottom_sheet()
+    MDTabs:
+        id: tabs
+        on_tab_switch: app.on_tab_switch(*args)
+
+
+<Tab>
+
+    MDIconButton:
+        id: icon
+        icon: root.icon
+        user_font_size: "48sp"
         pos_hint: {"center_x": .5, "center_y": .5}
 '''
 
 
-class MainApp(MDApp):
+class Tab(MDFloatLayout, MDTabsBase):
+    '''Class implementing content for a tab.'''
+
+
+class Example(MDApp):
+    icons = list(md_icons.keys())[15:30]
+
     def build(self):
         return Builder.load_string(KV)
 
-    def callback_for_menu_items(self, *args):
-        toast(args[0])
+    def on_start(self):
+        for tab_name in self.icons:
+            self.root.ids.tabs.add_widget(Tab(icon=tab_name))
 
-    def show_example_list_bottom_sheet(self):
-        bottom_sheet_menu = MDListBottomSheet()
-        for i in range(1, 11):
-            bottom_sheet_menu.add_item(
-                f"Standart Item {i}",
-                lambda x, y=i: self.callback_for_menu_items(
-                    f"Standart Item {y}"
-                ),
-            )
-        bottom_sheet_menu.open()
+    def on_tab_switch(
+        self, instance_tabs, instance_tab, instance_tab_label, tab_text
+    ):
+        '''
+        Called when switching tabs.
+
+        :type instance_tabs: <kivymd.uix.tab.MDTabs object>;
+        :param instance_tab: <__main__.Tab object>;
+        :param instance_tab_label: <kivymd.uix.tab.MDTabsLabel object>;
+        :param tab_text: text or name icon of tab;
+        '''
+        # get the tab icon.
+        count_icon = instance_tab.icon
+        # print it on shell/bash.
+        print(f"Welcome to {count_icon}' tab'")
 
 
-MainApp().run()
+Example().run()
