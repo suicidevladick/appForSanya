0b6c0e696fa50a6e80757e5774e7e92c26e710c7
suicidevladick <borisevich.vlad98@gmail.com>
2021-10-20 14:05:23 +0300

    autobuild

--- main.py
+++ main.py
@@ -1,10 +1,81 @@
 from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivymd.toast import toast
+from kivymd.uix.bottomsheet import MDListBottomSheet
 from kivymd.uix.label import MDLabel
+
+KV = '''
+MDScreen:
+
+    orientation:'vertical'
+
+    MDToolbar:
+        title: 'Bottom navigation'
+        md_bg_color: .2, .2, .2, 1
+        specific_text_color: 1, 1, 1, 1
+
+    MDBottomNavigation:
+        panel_color: .2, .2, .2, 1
+
+        MDBottomNavigationItem:
+            name: 'screen 1'
+            text: 'Sanya Loh'
+            icon: 'language-python'
+
+            MDLabel:
+                text: 'Python'
+                halign: 'center'
+
+        MDBottomNavigationItem:
+            name: 'screen 2'
+            text: 'C++'
+            icon: 'language-cpp'
+
+            MDLabel:
+                text: 'I programming of C++'
+                halign: 'center'
+
+        MDBottomNavigationItem:
+            name: 'screen 3'
+            text: 'JS'
+            icon: 'language-javascript'
+
+            MDLabel:
+                text: 'JS'
+                halign: 'center'
+
+
+    MDToolbar:
+        title: "Example BottomSheet"
+        pos_hint: {"top": 1}
+        elevation: 10
+
+    MDRaisedButton:
+        text: "Open list bottom sheet"
+        on_release: app.show_example_list_bottom_sheet()
+        pos_hint: {"center_x": .5, "center_y": .5}
+'''
+
+
 
 
 class MainApp(MDApp):
     def build(self):
-        return MDLabel(text="Hello, Vladick", halign="center")
+        return Builder.load_string(KV)
+
+    def callback_for_menu_items(self, *args):
+        toast(args[0])
+
+    def show_example_list_bottom_sheet(self):
+        bottom_sheet_menu = MDListBottomSheet()
+        for i in range(1, 11):
+            bottom_sheet_menu.add_item(
+                f"Standart Item {i}",
+                lambda x, y=i: self.callback_for_menu_items(
+                    f"Standart Item {y}"
+                ),
+            )
+        bottom_sheet_menu.open()
 
 
 MainApp().run()
