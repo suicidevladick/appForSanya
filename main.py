a5af79fc933a93ba949b049ee5f7cbc96a50d0b8
suicidevladick <borisevich.vlad98@gmail.com>
2021-10-21 10:08:24 +0300

    autobuild

--- main.py
+++ main.py
@@ -1,60 +1,127 @@
 from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty, ListProperty
 
 from kivymd.app import MDApp
-from kivymd.uix.tab import MDTabsBase
-from kivymd.uix.floatlayout import MDFloatLayout
-from kivymd.icon_definitions import md_icons
+from kivymd.theming import ThemableBehavior
+from kivymd.uix.list import OneLineIconListItem, MDList
 
 KV = '''
-MDBoxLayout:
-    orientation: "vertical"
+# Menu item in the DrawerList list.
+<ItemDrawer>:
+    theme_text_color: "Custom"
+    on_release: self.parent.set_color_item(self)
 
-    MDToolbar:
-        title: "Example Tabs"
-
-    MDTabs:
-        id: tabs
-        on_tab_switch: app.on_tab_switch(*args)
-
-
-<Tab>
-
-    MDIconButton:
+    IconLeftWidget:
         id: icon
         icon: root.icon
-        user_font_size: "48sp"
-        pos_hint: {"center_x": .5, "center_y": .5}
+        theme_text_color: "Custom"
+        text_color: root.text_color
+
+
+<ContentNavigationDrawer>:
+    orientation: "vertical"
+    padding: "8dp"
+    spacing: "8dp"
+
+    AnchorLayout:
+        anchor_x: "left"
+        size_hint_y: None
+        height: avatar.height
+
+        Image:
+            id: avatar
+            size_hint: None, None
+            size: "56dp", "56dp"
+            source: "data/logo/kivy-icon-256.png"
+
+    MDLabel:
+        text: "username"
+        font_style: "Button"
+        size_hint_y: None
+        height: self.texture_size[1]
+
+    MDLabel:
+        text: "borisevich.vlad98@gmail.com"
+        font_style: "Caption"
+        size_hint_y: None
+        height: self.texture_size[1]
+
+    ScrollView:
+
+        DrawerList:
+            id: md_list
+
+
+
+Screen:
+
+    MDNavigationLayout:
+
+        ScreenManager:
+
+            Screen:
+
+                BoxLayout:
+                    orientation: 'vertical'
+
+                    MDToolbar:
+                        title: "Home"
+                        elevation: 10
+                        left_action_items: [['menu', lambda x: nav_drawer.set_state("open")]]
+
+                    Widget:
+                        MDTextField:
+                            hint_text: "No helper text"
+                            halign: 'center'
+
+
+        MDNavigationDrawer:
+            id: nav_drawer
+
+            ContentNavigationDrawer:
+                id: content_drawer
 '''
 
-class Tab(MDFloatLayout, MDTabsBase):
-    '''Class implementing content for a tab.'''
+
+class ContentNavigationDrawer(BoxLayout):
+    pass
 
 
-class Example(MDApp):
-    icons = list(md_icons.keys())[15:30]
+class ItemDrawer(OneLineIconListItem):
+    icon = StringProperty()
+    text_color = ListProperty((0, 0, 0, 1))
 
+
+class DrawerList(ThemableBehavior, MDList):
+    def set_color_item(self, instance_item):
+        """Called when tap on a menu item."""
+
+        # Set the color of the icon and text for the menu item.
+        for item in self.children:
+            if item.text_color == self.theme_cls.primary_color:
+                item.text_color = self.theme_cls.text_color
+                break
+        instance_item.text_color = self.theme_cls.primary_color
+
+
+class appForSanya(MDApp):
     def build(self):
         return Builder.load_string(KV)
 
     def on_start(self):
-        for tab_name in self.icons:
-            self.root.ids.tabs.add_widget(Tab(icon=tab_name))
-
-    def on_tab_switch(
-        self, instance_tabs, instance_tab, instance_tab_label, tab_text
-    ):
-        '''
-        Called when switching tabs.
-
-        :type instance_tabs: <kivymd.uix.tab.MDTabs object>;
-        :param instance_tab: <__main__.Tab object>;
-        :param instance_tab_label: <kivymd.uix.tab.MDTabsLabel object>;
-        :param tab_text: text or name icon of tab;
-        '''
-        # get the tab icon.
-        count_icon = instance_tab.icon
-        # print it on shell/bash.
-        print(f"Welcome to {count_icon}' tab'")
+        icons_item = {
+            "folder": "My files",
+            "account-multiple": "Shared with me",
+            "star": "Starred",
+            "history": "Recent",
+            "checkbox-marked": "Shared with me",
+            "upload": "Upload",
+        }
+        for icon_name in icons_item.keys():
+            self.root.ids.content_drawer.ids.md_list.add_widget(
+                ItemDrawer(icon=icon_name, text=icons_item[icon_name])
+            )
 
 
-Example().run()
+appForSanya().run()
